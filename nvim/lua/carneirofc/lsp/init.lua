b34aaeb8cffef1f2628330e9fa7a3006c47f0df6
Cláudio Ferreira Carneiro <claudiofcarneiro@hotmail.com>
2023-04-13 22:47:09 -0300

    feat(nvim/lsp): comments and keymaps

--- nvim/lua/carneirofc/lsp/init.lua
+++ nvim/lua/carneirofc/lsp/init.lua
@@ -2,7 +2,20 @@ local M = {}
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
+    -- if client.name == "omnisharp" then
+    --     client.server_capabilities.semanticTokensProvider.legend = {
+    --         tokenModifiers = { "static" },
+    --         tokenTypes = { "comment", "excluded", "identifier", "keyword", "keyword", "number", "operator", "operator",
+    --             "preprocessor", "string", "whitespace", "text", "static", "preprocessor", "punctuation", "string",
+    --             "string", "class", "delegate", "enum", "interface", "module", "struct", "typeParameter", "field",
+    --             "enumMember", "constant", "local", "parameter", "method", "method", "property", "event", "namespace",
+    --             "label", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml",
+    --             "xml", "xml", "xml", "xml", "xml", "xml", "xml", "xml", "regexp", "regexp", "regexp", "regexp", "regexp",
+    --             "regexp", "regexp", "regexp", "regexp" }
+    --     }
+    -- end
+
     -- In this case, we create a function that lets us more easily define mappings specific
     -- for LSP related items. It sets the mode, buffer and description for us each time.
     local nmap = function(keys, func, desc)
@@ -13,32 +26,31 @@ local on_attach = function(_, bufnr)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-    nmap('<A-S-f>', vim.lsp.buf.format, '[A-S-f]ormat')
+    nmap('<leader>rn', vim.lsp.buf.rename, 'Rename')
+    nmap('<leader>ca', vim.lsp.buf.code_action, 'Code Action')
+    nmap('<A-S-f>',
+        function()
+            vim.lsp.buf.format({ async = false, timeout_ms = 1000 })
+        end, 'Format')
 
-    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    nmap('gd', vim.lsp.buf.definition, 'Go to definition')
+    nmap('gr', require('telescope.builtin').lsp_references, 'Go to reference')
+    nmap('gI', vim.lsp.buf.implementation, 'Go to implementation')
+    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
+    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace Symbols')
+
     -- See `:help K` for why this keymap
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
     -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
+    nmap('gD', vim.lsp.buf.declaration, 'Go to Declaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, 'Workspace Add Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, 'Workspace Remove Folder')
+    nmap('<leader>wl',
+        function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, 'Workspace List Folders')
 end
 
 local function setup_null_ls()
@@ -52,8 +64,14 @@ local function setup_null_ls()
             null_ls.builtins.formatting.black,
             null_ls.builtins.formatting.clang_format,
             null_ls.builtins.formatting.isort,
-            null_ls.builtins.formatting.prettier,
             null_ls.builtins.formatting.jq,
+            null_ls.builtins.formatting.prettier,
+            null_ls.builtins.diagnostics.shellcheck.with {
+                filetypes = { 'sh', 'bash' },
+            },
+            null_ls.builtins.formatting.shfmt.with {
+                filetypes = { 'sh', 'bash' },
+            },
         }
     })
 end
@@ -61,7 +79,7 @@ end
 local function setup_lsp_zero()
     local lsp = require('lsp-zero').preset({})
 
-    lsp.ensure_installed({ 'clangd', 'gopls', 'pyright', 'rust_analyzer', 'tsserver', 'csharp_ls' })
+    lsp.ensure_installed({ 'clangd', 'gopls', 'pyright', 'tsserver', 'csharp_ls' })
 
     lsp.on_attach(on_attach)
 
@@ -70,10 +88,9 @@ local function setup_lsp_zero()
             ['lua_ls'] = { 'lua' },
             ['rust_analyzer'] = { 'rust' },
             ['null-ls'] = { 'python', 'javascript', 'typescript', 'cpp', 'c' }
-        }
+        },
+        format_opts = { async = false, timeout_ms = 1000 }
     })
-    -- vim.lsp.buf.format()
-
     -- Configure language servers before lsp setup
     require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
