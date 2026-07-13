c523c6502c92ef499ce6db1bba8de567d7d559e7
carneirofc <claudiofcarneiro@hotmail.com>
2026-07-13 20:47:59 -0300

    refactor(nvim): remove old config path

--- nvim/lua/carneirofc/init.lua
+++
@@ -1,7 +0,0 @@
-require('carneirofc.packer').setup()
-require('carneirofc.settings').setup()
-require('carneirofc.keymaps').setup()
-require('carneirofc.plugins').setup()
-
--- load last...
-require('carneirofc.lsp').setup()
--- nvim/lua/carneirofc/plugins/setup-black.lua
+++
@@ -1,5 +0,0 @@
-local M = {}
-function M.setup()
-    vim.cmd([[autocmd BufWritePre *.py execute ':Black']])
-end
-return M
